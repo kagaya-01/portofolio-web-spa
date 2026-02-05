db05310fa28a1fa54bec4dc205c94f162cd6db88
kagaya-01 <hibbanibrahim09@gmail.com>
2026-02-05 15:46:30 +0700

    make it SPA

--- global/script/script.js
+++ global/script/script.js
@@ -1,3 +1,4 @@
+// SIDEBAR
 let isOpened = false;
 function sidebar() { 
     const sidebarContainer = document.querySelector(".sidebar");
@@ -30,4 +31,23 @@ function sidebar() {
             sidebarUl.style.setProperty('display', 'none');
         }, 100);
     }
+}
+
+// SEND EMAIL
+function sendEmail(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value || "No Subject";
+    const message = document.getElementById("message").value;
+
+    // ubah ke alamat email kamu
+    const receiver = "hibbanyama@gmail.com";
+
+    // encode agar teks terbaca di URL
+    const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(receiver)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent("Nama: " + name + "\nEmail: " + email + "\n\nPesan:\n" + message)}`;
+
+    // buka Gmail di tab baru
+    window.open(gmailLink, "_blank");
 }
